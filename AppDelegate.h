c04a3dc0908c330467bcbcc2ba4c9f1462f4b645
cambrussel <cambrussel@Adrianas-MacBook-Pro.local>
2015-07-21 19:04:26 -0600

    Tarea4

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tarea4
+//
+//  Created by Adriana  Cambronero on 7/19/15.
+//  Copyright (c) 2015 Adriana Cambronero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tarea4
+//
+//  Created by Adriana  Cambronero on 7/19/15.
+//  Copyright (c) 2015 Adriana Cambronero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
